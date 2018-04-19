f25cc97df4ecb6c06ab7017f7b6f8988591f40a7
Christian Solis-Shepperson <32067731+SoShepperson@users.noreply.github.com>
2018-04-19 13:48:28 -0400

    Added HashMap Lookup Table/ Partial Priority Queue

--- HuffmanCoding.java
+++ HuffmanCoding.java
@@ -2,18 +2,20 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.PriorityQueue;
+import java.util.HashMap;
 
 public class HuffmanCoding {
-	
-	PriorityQueue<String> pQueue;
-	public static String getTextFromFile(String filename){
+
+	PriorityQueue<Node> pQueue;
+
+	public static String getTextFromFile(String filename) {
 		BufferedReader br = null;
 		FileReader fr = null;
 		String textInFile = "";
 
 		try {
 
-			//br = new BufferedReader(new FileReader(FILENAME));
+			// br = new BufferedReader(new FileReader(FILENAME));
 			fr = new FileReader(filename);
 			br = new BufferedReader(fr);
 
@@ -46,15 +48,39 @@ public class HuffmanCoding {
 		}
 		return textInFile;
 	}
+
+	public static HashMap<String, Integer> getLookupTable(String s) {
+		HashMap<String, Integer> lookupTable = new HashMap<String, Integer>();
+
+		for (char c : s.toCharArray()) {
+
+			if (lookupTable.containsKey(Character.toString(c))) {
+				// Check to make sure this increments Integer value of existing key
+				//Call should update existing key and increment value
+				lookupTable.put(Character.toString(c), lookupTable.get(Character.toString(c)) + 1);
+			} else {
+				lookupTable.put(Character.toString(c), 1);
+			}
+
+		}
+
+		return lookupTable;
+	}
+
 	public static void huffmanEncode(String filename) {
 		// TODO Auto-generated method stub
 		String rawText = getTextFromFile(filename);
+		HashMap<String, Integer> table = getLookupTable(rawText);
+		
+		//create nodes using the hashmap? weight will be the value in hashmap
+		for(int i = 0; i < table.size();i++) {
+			//store nodes created in priority queue
+			//PriorityQueue instance variable should be initalized to contain a Leaf Node for each different character
+		}
+		
+		
 		
 
-		
 	}
-
-
-
 
 }
